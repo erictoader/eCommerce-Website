7801781b0264b1e3cd6337416d62f7749fc84180
Bogdan <pupezab@yahoo.com>
2022-12-26 12:36:17 +0200

    login component

--- eComFrontEnd/angular-ecommerce/src/app/components/login/login.component.ts
+++ eComFrontEnd/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppValidators } from 'src/app/validators/app-validators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  loginFormGroup: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.formBuilder.group({
+      loginData: this.formBuilder.group({
+        username: new FormControl("",[
+          Validators.required,
+        ]),
+        password: new FormControl("",[
+          Validators.required,
+        ]),
+      })
+    });
+  }
+
+  get username() {return this.loginFormGroup.get("loginData.username")!;}
+  get password() {return this.loginFormGroup.get("loginData.password")!;}
+
+  onLogin() {
+    console.log("Handle login button");
+
+    if (this.loginFormGroup.invalid){
+      this.loginFormGroup.markAllAsTouched();
+    }
+    console.log(this.loginFormGroup.get("loginData")?.value);
+  }
 }
