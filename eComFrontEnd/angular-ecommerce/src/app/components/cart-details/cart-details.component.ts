9a6756f9507c8b8b0792cc73585b64f7663e23ef
Bogdan <pupezab@yahoo.com>
2023-01-17 22:35:06 +0200

    fe update

--- eComFrontEnd/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ eComFrontEnd/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartItem } from 'src/app/models/cart-item';
 import { CartService } from 'src/app/services/cart.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -14,7 +16,7 @@ export class CartDetailsComponent {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private router: Router, private loginService: LoginService) { }
   
   ngOnInit(): void {
     this.listCartDetails();
@@ -45,6 +47,18 @@ export class CartDetailsComponent {
 
   removeFromCart(cartItem: CartItem){
     this.cartService.removeFromCart(cartItem);
+  }
+
+  isLoggedIn(){
+    return this.loginService.getUser() != null;
+  }
+
+  gotoCheckout(){
+    if(this.isLoggedIn()){
+      this.router.navigate(["/checkout"]);
+    }else{
+      this.router.navigate(["/login"]);
+    }
   }
 
 }
